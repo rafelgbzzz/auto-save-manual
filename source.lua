3dcae22df858334b06b81d4261d68ee93791172c
rafelgbzzz <rafelseptaa@gmail.com>
2025-10-12 12:44:56 +0700

    source.lua

---
+++ source.lua
@@ -0,0 +1,10 @@
+-- icons.lua custom "ZeroKage"
+local Icons = {
+    ["ZeroKage"] = {123456789, {48, 48}, {0, 0}},  -- ID, ukuran, posisi
+    ["rewind"] = {123456789, {48, 48}, {0, 0}},
+    ["fuel"] = {123456789, {48, 48}, {0, 0}},
+    ["square-arrow-out-up-right"] = {123456789, {48, 48}, {0, 0}},
+    -- semua ikon lain juga diarahkan ke ZeroKage
+}
+
+return Icons
